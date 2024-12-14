66c221b22413707ada0812f2adb4d6f2da454b4a
Tejasree-S27 <stejasree348@gmail.com>
2024-12-14 11:09:11 +0530

    Add files via upload

---
+++ MultiLevelInheritance.java
@@ -0,0 +1,30 @@
+import java.util.*;
+interface A
+{
+public void r();
+} 
+interface B
+{
+    public void r1();
+}
+class C implements A,B
+{
+public void r()
+{
+    System.out.println("Hello");
+}
+public void r1()
+{
+    System.out.println("Hii");
+}
+} 
+public class MultiLevelInheritance
+{
+    public static void main(String args[])
+    {
+        C h1=new C();
+        h1.r();
+        h1.r1();
+
+    }
+}

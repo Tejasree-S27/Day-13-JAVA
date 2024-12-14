adbc7369e39eef3734d58e2c09f00a9d781fb9f8
Tejasree-S27 <stejasree348@gmail.com>
2024-12-14 15:08:48 +0530

    Add files via upload

---
+++ Try_Catch_Finally.java
@@ -0,0 +1,25 @@
+public class Try_Catch_Finally
+  {
+    public static void main(String args[])
+    {
+      //try-catch
+      try{
+        int d=90/0;
+      }
+      catch(ArithmeticException v)
+      {
+        System.out.println("Bad Request");
+      }
+      try{
+        int d=90/0;
+      }
+      catch(ArithmeticException v)
+      {
+        System.out.println("Catch block is running");
+      }
+      finally
+      {
+        System.out.println("Finally is running");
+      }
+    }
+  }
